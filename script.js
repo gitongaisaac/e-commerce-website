c2f92c2fcab4e834c04a4cf5e68af1995888fb10
isaac <isaacggitonga@gmail.com>
2022-05-19 17:42:20 +0300

    Done with the website html and css except responsiveness

--- script.js
+++ script.js
@@ -1,17 +1,202 @@
-const vets = document.querySelectorAll('.vet');
-vets.forEach(vet => {
-    const vetText = vet.textContent.trim().length;
-    if(vetText >= 16) {
-        const vetSlice = vet.textContent.trim();
-        const newVet = vetSlice.substring(0, 16) + '...';
-        vet.textContent = newVet;
-    }
-});
+/*
+================================================================================================================================
+Dom Elements
+================================================================================================================================
+*/
+/* ============= Image divs ================ */
+const jacket = document.querySelector('.jacket');
+const shirt = document.querySelector('.shirt');
+const trench = document.querySelector('.trench');
+const skirt = document.querySelector('.skirt');
+const casual= document.querySelector('.casual');
+const pocketWatch = document.querySelector('.pocket-watch');
+const wristWatch = document.querySelector('.wrist-watch');
+const party = document.querySelector('.party');
+const coat = document.querySelector('.coat');
+const sports = document.querySelector('.sports');
+const formal = document.querySelector('.formal');
+const shorts = document.querySelector('.shorts');
 
+
+/* ============= Other Elements ================ */
 const views = document.querySelectorAll('.view');
+const viewImgs = document.querySelectorAll('.view-img');
+
+
+
+/*
+================================================================================================================================
+Images
+================================================================================================================================
+*/
+const jacketImg = "url('images/products/jacket-3.jpg')";
+const jacketImgHover = "url('images/products/jacket-4.jpg')";
+
+const shirtImg = "url('images/products/shirt-1.jpg')";
+const shirtImgHover = "url('images/products/shirt-2.jpg')";
+
+const trenchImg = "url('images/products/jacket-5.jpg')";
+const trenchImgHover = "url('images/products/jacket-6.jpg')";
+
+const skirtImg = "url('images/products/clothes-3.jpg')";
+const skirtImgHover = "url('images/products/clothes-4.jpg')";
+
+const casualImg = "url('images/products/shoe-2.jpg')";
+const casualImgHover = "url('images/products/shoe-2_1.jpg')";
+
+const pocketWatchImg = "url('images/products/watch-3.jpg')";
+const pocketWatchImgHover = "url('images/products/watch-4.jpg')";
+
+const wristWatchImg = "url('images/products/watch-1.jpg')";
+const wristWatchImgHover = "url('images/products/watch-2.jpg')";
+
+const partyImg = "url('images/products/party-wear-1.jpg')";
+const partyImgHover = "url('images/products/party-wear-2.jpg')";
+
+const coatImg = "url('images/products/jacket-1.jpg')";
+const coatImgHover = "url('images/products/jacket-2.jpg')";
+
+const sportsImg = "url('images/products/sports-2.jpg')";
+const sportsImgHover = "url('images/products/sports-4.jpg')";
+
+const formalImg = "url('images/products/shoe-1.jpg')";
+const formalImgHover = "url('images/products/shoe-1_1.jpg')";
+
+const shortsImg = "url('images/products/shorts-1.jpg')";
+const shortsImgHover = "url('images/products/shorts-2.jpg')";
+
+
+/*
+================================================================================================================================
+Arrays
+================================================================================================================================
+*/
+const imgTags = [jacket, shirt, trench, skirt, casual, pocketWatch, 
+                 wristWatch, party, coat, sports, formal, shorts];
+
+const images = [jacketImg, shirtImg, trenchImg, skirtImg, casualImg, 
+                pocketWatchImg, wristWatchImg, partyImg, coatImg, 
+                sportsImg, formalImg, shortsImg];
+
+const imageHovers = [jacketImgHover, shirtImgHover, trenchImgHover, 
+                    skirtImgHover, casualImgHover, pocketWatchImgHover, 
+                    wristWatchImgHover, partyImgHover, coatImgHover, 
+                    sportsImgHover, formalImgHover, shortsImgHover];
+
+/* ============= style syntax ================ */
+// const styleSyntax = style.backgroundImage;
+
+
+    // const jacketa = "url('images/products/jewellery-1.jpg')";
+    // jacket.style.backgroundImage = jacketa;
+
+
+
+/*
+================================================================================================================================
+UI Class
+================================================================================================================================
+*/
+class UI {
+    static titleSubString() {
+        const vets = document.querySelectorAll('.vet');
+
+        vets.forEach(vet => {
+        const vetText = vet.textContent.trim().length;
+            if(vetText >= 16) {
+                const vetSlice = vet.textContent.trim();
+                const newVet = vetSlice.substring(0, 16) + '...';
+                vet.textContent = newVet;
+                return vet.textContent = newVet;
+            }
+        });
+    }
+
+
+
+    static images() {
+        imgTags.forEach(imgTag => {
+            UI.mouseOver(imgTag);
+            UI.mouseLeave(imgTag);
+        });
+        
+        images.forEach(image => {
+            UI.mouseOver(image);
+        });
+        
+        imageHovers.forEach(imageHover => {
+            UI.mouseLeave(imageHover);
+        });
+    }
+
+
+
+    static mouseOver(imgTag, image) {
+        imgTag.style.backgroundColor = image;
+    }
+
+
+    static mouseLeave(imgTag, imageHover) {
+        imgTag.style.backgroundColor = imageHover;
+    }
+}
+
+
+/*
+================================================================================================================================
+On Loading the page
+================================================================================================================================
+*/
+function onLoading() {
+    UI.titleSubString();
+}
+
+
+/*
+================================================================================================================================
+Event DOMConentLoaded
+================================================================================================================================
+*/
+document.addEventListener('DOMContentLoaded', (e) => {
+    onLoading();
+})
+
+
+
+
+/*
+================================================================================================================================
+Eventlistener
+================================================================================================================================
+*/
+/* ============= mouse over ================ */
 views.forEach(view => {
-    // onmouseover
     view.addEventListener('mouseover', (e) => {
-        console.log('it works');
+    // view.style.boxShadow = 'var(--shadow-4)';
+    view.style.boxShadow = '0 20px 25px -5px rgba(0, 0, 0, 0.1), -10px 10px 10px 5px rgba(0, 0, 0, 0.04)';
+    UI.mouseOver();
     });
 });
+
+
+/* ============= mouse leave ================ */
+views.forEach(view => {
+    view.addEventListener('mouseleave', (e) => {
+    view.style.boxShadow = 'var(--shadow-1)';
+    UI.mouseLeave(); 
+    });
+});
+
+
+
+/*
+================================================================================================================================
+filter
+================================================================================================================================
+*/
+
+/*
+================================================================================================================================
+filter
+================================================================================================================================
+*/
